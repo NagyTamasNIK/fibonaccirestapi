49475fa1d5c15f15b347e6a4bc5ac58b934efb56
NagyTamasNIK <pokerarc007@gmail.com>
2025-03-10 12:02:52 +0100

    fibonacci back

--- src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
+++ src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
@@ -4,8 +4,27 @@ import org.springframework.stereotype.Service
 
 @Service
 class FibonacciService {
+    /*fun fibonacci(n: Int): Int {
+        for(i in 0..n){
+
+        }
+        return if (n == 1) 1
+        else
+        //else 0 // TODO instead of this logic implement fibonacci
+    }*/
     fun fibonacci(n: Int): Int {
-        return if (n == 1) 0
-        else 0 // TODO instead of this logic implement fibonacci
+        if (n <= 1) return n
+
+        var a = 0
+        var b = 1
+        var fib = 0
+
+        for (i in 2..n) {
+            fib = a + b
+            a = b
+            b = fib
+        }
+
+        return fib
     }
 }
